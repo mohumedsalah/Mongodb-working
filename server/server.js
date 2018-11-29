2393cee8b36f5273a3de39102bde4d29fd3792f0
Mohumed Salah <mohumed.s.omar@gmail.com>
2018-11-29 15:00:47 +0200

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express =require('express')
 var bodyparser = require('body-parser')
 
-
+const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
@@ -41,6 +41,25 @@ app.get('/todos',(req, res)=>{
     },(e)=>{
         res.status(400).send(e);
     })
+})
+app.get('/todos/:id',(req, res)=>{
+    
+    var id = req.params.id;
+    //console.log(id);
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send("id is not vaild");
+    }
+    
+    Todo.findById(new ObjectID (id)).then((ret)=>{
+        if(!ret){
+            return res.status(404).send("Not found this id in database");
+        }
+        return res.send(ret);
+    }, (err)=>{
+        return res.status(400).send(err);
+    })
+    
+
 })
 
 app.listen(3000, ()=>{
