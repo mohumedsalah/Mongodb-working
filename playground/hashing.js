ae1b6827b183ee5d143fbd96c898e7c5ae324fdf
Mohumed Salah <mohumed.s.omar@gmail.com>
2018-12-01 13:47:58 +0200

    hash password befor save it

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const {SHA256} =require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data ={
-    id:10
-}
+var password = 'aaaaa'
 
 
-var token = jwt.sign(data,'123abc')
-console.log(token);
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+})
 
-var decoded = jwt.verify(token  ,'123abc')
-console.log(decoded); 
+var hashed = "$2a$10$vPL5xM15aGs4XFMnmm0yO.BGStju9A8ptSNCC/l9dwHF7mQ9K";
+
+
+bcrypt.compare(password, hashed,(err, res) =>{
+    console.log(res);
+})
+// var data ={
+//     id:10
+// }
+
+
+// var token = jwt.sign(data,'123abc')
+// console.log(token);
+
+// var decoded = jwt.verify(token  ,'123abc')
+// console.log(decoded); 
 
 // var message ="iam user number 3";
 
